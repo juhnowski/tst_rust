a9ff756f82b9ee02dea1b6b375afdc18493a96ec
juhnowski <juhnowski@gmail.com>
2020-11-01 10:26:57 +0300

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+
+use log::{error, info, warn, LevelFilter};
+use log4rs::{
+    append::console::ConsoleAppender,
+    config::{Appender, Root},
+    encode::json::JsonEncoder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout: ConsoleAppender = ConsoleAppender::builder()
+        .encoder(Box::new(JsonEncoder::new()))
+        .build();
+    let log_config = log4rs::config::Config::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+        .unwrap();
+    log4rs::init_config(log_config).unwrap();
+
+    info!("App started");
+/*    warn!("Warn log with value {}", "test");
+    error!("ERROR!");
+*/
+
 }
