c090313bb4526a22d45c7aa29fce9f102c58ee23
juhnowski <juhnowski@gmail.com>
2020-11-01 12:55:32 +0300

    add gtk

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ use log4rs::{
     encode::json::JsonEncoder,
 };
 
+extern crate gtk;
+extern crate gio;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+
+use gtk::{ButtonsType, DialogFlags, MessageType, MessageDialog, Window};
+
+use std::env;
+
 fn main() {
     let stdout: ConsoleAppender = ConsoleAppender::builder()
         .encoder(Box::new(JsonEncoder::new()))
@@ -20,5 +30,35 @@ fn main() {
 /*    warn!("Warn log with value {}", "test");
     error!("ERROR!");
 */
+
+if gtk::init().is_err() {
+    error!("Failed to initialize GTK.");
+    return;
+}
+let glade_src = include_str!("builder_basics.glade");
+let builder = gtk::Builder::from_string(glade_src);
+
+let window: gtk::Window = builder.get_object("window1").unwrap();
+let button: gtk::Button = builder.get_object("button1").unwrap();
+let dialog: gtk::MessageDialog = builder.get_object("messagedialog1").unwrap();
+
+let revert: gtk::Button = builder.get_object("button_revert").unwrap();
+let host: gtk::Entry  = builder.get_object("host").unwrap();
+let port: gtk::Entry  = builder.get_object("port").unwrap();
+
+button.connect_clicked(move |_| {
+    dialog.run();
+    dialog.hide();
+});
+
+revert.connect_clicked(move |_| {
+    host.set_text("127.0.0.1")
+    port.set_text("7001")
+});
+
+
+window.show_all();
+
+gtk::main();
 
 }
